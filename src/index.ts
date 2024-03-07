167cdff7dad1130c07fae4d7b38ce02463f5ebcd
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-03-07 20:33:48 -0300

    feat: add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { router } from './app/routes';
+import { cors } from './app/middlewares/cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors);
 app.use(router);
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
